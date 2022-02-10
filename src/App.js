c4728dff44596fd965dfdb6b4d1a7d6a717e092b
Victor <victor7stepanov@ya.ru>
2022-02-10 20:00:08 +0400

    Complete circle layout and implement appearing circle after press spacebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {useEffect, useState} from "react";
 
-export default function App() {
-  return (
-      <div>
+export default function App(props) {
+  const [circle, setCircle] = useState(null);
 
+  const circleTemplate = (
+      <div className={'circle'}>
+          <div className={'circle__value'}>
+              {props.value}
+          </div>
       </div>
+  )
+
+  const handleSpace = (event) => {
+      if (event.code === 'Space') {
+          setCircle(circleTemplate);
+      }
+  }
+
+  useEffect(() => {
+      document.addEventListener('keydown', handleSpace);
+      console.log('effect')
+      return () => {
+          document.removeEventListener('keydown', handleSpace)
+      }
+  }, [])
+
+  return (
+      circle
   );
 }
 
